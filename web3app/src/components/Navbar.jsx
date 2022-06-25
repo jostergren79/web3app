ab7f74420e4355e248eaf8be425870a5638290a3
Jason Ostergren <jason@Jasons-MacBook-Pro.local>
2022-06-25 12:26:21 -0500

    updating web3app with additional libraries and css

--- web3app/src/components/Navbar.jsx
+++ web3app/src/components/Navbar.jsx
@@ -1,6 +1,15 @@
+import { HiMenutAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from "../../images/logo.png";
+import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div
+    className='w-full flex md:justify-center justify-between items-center p-4'
+    >
+    Navbar
+    </div>
   )
 }
 
