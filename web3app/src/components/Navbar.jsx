6c4ea9f84a44c6b01074b6c3b362470007f525b5
Jason Ostergren <jason@Jasons-MacBook-Pro.local>
2022-06-28 16:54:46 -0500

    logo work

--- web3app/src/components/Navbar.jsx
+++ web3app/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import { HiMenutAlt4 } from "react-icons/hi"
+import { HiMenuAlt4 } from "react-icons/hi"
 import { AiOutlineClose } from "react-icons/ai"
-import logo from "../../images/logo.png"
+import logo from "../../images/ŌṃTech-logos_white.png"
 import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime"
 
 const Navbar = () => {
   return (
-    <div
-    className='w-full flex md:justify-center justify-between items-center p-4'
-    >
-    Navbar
-    </div>
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className="md:flex-[0.5] flex-initial justify-center items-center p">
+        <img src={logo} alt="logo" className="w-32" />
+      </div>
+    </nav>
   )
 }
 
